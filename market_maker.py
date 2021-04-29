dd592ddb22db6a33cdc95c49546ff0448149340f
Ubuntu <ubuntu@reddscrapes.northamerica-northeast1-a.c.coindex-ai-v0.internal>
2021-04-29 22:41:53 +0000

    prod

--- market_maker.py
+++ market_maker.py
@@ -982,7 +982,7 @@ class MarketMaker( object ):
         except Exception as e:
             pprint('m_message ' + str(e))#PrintException()
     def dorestart( self ):
-        sleep(40)
+        sleep(240)
         self.restart()
     def run_first( self, client ):
         
