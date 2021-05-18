5d871bb477b81951d829cfaf7b2a6c861861fb8b
Your Name <you@example.com>
2021-05-18 02:34:01 -0300

    margin

--- conf.json.ex
+++ conf.json.ex
@@ -1,3 +1,3 @@
 {"apikey": "xxxx",
 "apisecret": "xxxx",
-"lev": 6, "TP": 10, "SL": -500, "max_skew_mult": 1.75, "qty_div": 3}
+"lev": 6, "TP": 10, "SL": -500, "max_skew_mult": 1.75, "qty_div": 0.3}
